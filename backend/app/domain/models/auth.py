a2567babb6221588166260bd1463f1fd7bf7d346
CTI Guarulhos <ctiguarulhos@ifsp.edu.br>
2024-09-05 21:42:13 -0300

    feat: adiciona model de autenticacao e service

---
+++ backend/app/domain/models/auth.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
